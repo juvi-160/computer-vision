68b73d0ea75cf9addc3668b8639c5bc3830fdaaf
juvi-160 <juveriayameen160@gmail.com>
2026-09-22 20:56:46 +0530

    Import drawRect utility and update drawing logic in detect function

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
 import "./App.css";
 // 2. TODO - Import drawing utility here
-// e.g. import { drawRect } from "./utilities";
+import { drawRect } from "./utilities";
 
 function App() {
   const webcamRef = useRef(null);
@@ -54,7 +54,7 @@ function App() {
       const ctx = canvasRef.current.getContext("2d");
 
       // 5. TODO - Update drawing utility
-      // drawSomething(obj, ctx)  
+      drawRect(obj, ctx);
     }
   };
 
