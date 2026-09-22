21adbbf59ba458cc709a7b6f541f56d0dd40b6ca
juvi-160 <juveriayameen160@gmail.com>
2026-09-22 22:18:16 +0530

    ./

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+// Import dependencies
+import React, { useRef, useEffect } from "react";
+// import * as tf from "@tensorflow/tfjs";
+// 1. TODO - Import required model here
+// e.g. import * as tfmodel from "@tensorflow-models/tfmodel";
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-backend-webgl";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import Webcam from "react-webcam";
+import "./App.css";
+// 2. TODO - Import drawing utility here
+import { drawRect } from "./utilities";
+
+
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
